f03271d3088cf8abcfb94ef951a3cf45563f861b
Matthias Rudigier <mat.rudigier@gmail.com>
2022-04-01 21:22:28 +0200

    Bugfixes for previous commit

--- ScintillatorPulse.py
+++ ScintillatorPulse.py
@@ -49,7 +49,7 @@ class ScintillatorPulse:
         max = value
         max_t = t
 
-  def getRealRisetime(self):
+  def getRealRiseTime(self):
     peak = self.getMax()
     step = (peak[0] - self.limitL)/10000
     havet10 = 0
@@ -62,18 +62,18 @@ class ScintillatorPulse:
       elif (value > peak[1]*0.9) :
         return (t - t10)
 
-   def getRealDecayTime(self):
-     peak = self.getMax()
-     step = (self.limitR - peak[0])/10000
-     havet90 = 0
-     t90 = 0.0
-     for t in np.arange(peak[0], self.limitR, step):
-       value = self.__pulseShape(t)
-       if (havet90 != 0 and value < peak[1]*0.9) :
-         havet90 = 1
-         t90 = t
-       elif (value < peak[1]*0.1) :
-         return (t - t90)
+  def getRealDecayTime(self):
+    peak = self.getMax()
+    step = (self.limitR - peak[0])/10000
+    havet90 = 0
+    t90 = 0.0
+    for t in np.arange(peak[0], self.limitR, step):
+      value = self.__pulseShape(t)
+      if (havet90 != 0 and value < peak[1]*0.9) :
+        havet90 = 1
+        t90 = t
+      elif (value < peak[1]*0.1) :
+        return (t - t90)
 
   def getArea(self, tLow=0.0, tHigh=0.0, nSamples=0):
     area = 0.0
